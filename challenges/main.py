8e9f7558f61e272288d77a62d5011d1d262d760e
emmanuelnapoleon <emmanuelnapoleon13@gmail.com>
2026-08-31 14:30:20 +0100

    Complete: Fix the Character Counter

--- challenges/main.py
+++ challenges/main.py
@@ -1,8 +1,8 @@
-def build_countdown(start):
-    countdown = []
-    # TODO: use a for loop with range() to count down from `start` to 1,
-    # appending each number to `countdown`
-    for time in range(start,0,-1):
-        countdown.append(time)
-    return countdown
-print(build_countdown(1))
+def count_character(text, target):
+    count = 0
+
+    for char in text:
+        if char in target:
+            count += 1
+    return count
+print(count_character("BANANA","a"))
