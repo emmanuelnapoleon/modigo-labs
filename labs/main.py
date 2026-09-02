df437e8995176f7c49f4e7b0f74795886b9e47f8
emmanuelnapoleon <emmanuelnapoleon13@gmail.com>
2026-09-02 12:52:43 +0100

    Complete: A group of friends wants to split a restaurant bill evenly,

--- labs/main.py
+++ labs/main.py
@@ -1,6 +1,16 @@
-def swap_values(a, b):
-    new_a = b
-    new_b = a
-    return (new_a, new_b)
+# No starter code provided — write the full function yourself.
+# Function name: split_bill
+# Parameters: bill_amount, tip_percent, people
+# Must return: each person's share, rounded to 2 decimal places
 
-print(swap_values("hi","bye"))
+def split_bill(bill_amount, tip_percent,people):
+
+    tip_amount = bill_amount*(tip_percent/100)
+    ground_total = bill_amount + tip_amount
+    person_share = ground_total/people
+
+    return round(person_share,2)
+
+print(split_bill(100,10,2))
+print(split_bill(60,20,3))
+print(split_bill(50,0,1))
